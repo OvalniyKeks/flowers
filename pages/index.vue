b2b677c1cf98004d547003ca00216d811fceae80
jalenix <babaevwtf@mail.ru>
2023-01-26 06:53:32 +0300

    add section card

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,25 @@
 <template>
   <main>
     <section class="section section-main">
-        <div class="section-main__left">
-          <h1>Flowers, <br>what the world</br> needs</h1>
-          <div class="section-title">Browse between hounders of flowers</div>
-          <button class="section-button">Browse</button>
-        </div>
-        <div class="section-main__right">
-          <img src="~/assets/image/flower_1.png" alt="">
-          <img src="~/assets/image/flower_2.png" alt="">
-          <img src="~/assets/image/flower_3.png" alt="">
-          <img src="~/assets/image/flower_4.png" alt="">
-          <img src="~/assets/image/flower_5.png" alt="">
-          <img src="~/assets/image/flower_6.png" alt="">
-        </div>
+      <div class="section-main__left">
+        <h1>Flowers, <br>what the world</br> needs</h1>
+        <div class="section-title">Browse between hounders of flowers</div>
+        <button class="section-button">Browse</button>
+      </div>
+      <div class="section-main__right">
+        <img src="~/assets/image/flower_1.png" alt="">
+        <img src="~/assets/image/flower_2.png" alt="">
+        <img src="~/assets/image/flower_3.png" alt="">
+        <img src="~/assets/image/flower_4.png" alt="">
+        <img src="~/assets/image/flower_5.png" alt="">
+        <img src="~/assets/image/flower_6.png" alt="">
+      </div>
+    </section>
+    <section class="card card-selers">
+        <h2>Best selers</h2>
+      <div class="card-content">
+
+      </div>
     </section>
   </main>
 </template>
